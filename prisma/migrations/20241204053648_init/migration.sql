285dd32cc6325b311dc0db9434de8ad7a7697266
Chris Lane Jones <chrislanejones@gmail.com>
2024-12-04 02:57:57 -0500

    Added Prisma to the Search Bar  and dev.db

---
+++ prisma/migrations/20241204053648_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "video" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "star" TEXT NOT NULL
+);
--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,20 +1,37 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Search } from "lucide-react";
 import { FilterDropdown } from "./filter-dropdown";
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const params = new URLSearchParams(searchParams);
+    params.set("query", searchTerm);
+    router.push(`/?${params.toString()}`);
+  };
+
   return (
     <div className="flex items-center space-x-2">
-      <Input
-        type="text"
-        placeholder="Search videos..."
-        className="w-full max-w-xs bg-secondary text-secondary-foreground"
-      />
-      <Button type="submit" size="sm" variant="secondary">
-        <Search className="h-4 w-4" />
-        <span className="sr-only">Search</span>
-      </Button>
+      <form onSubmit={handleSearch}>
+        <Input
+          type="text"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search..."
+          className="w-full max-w-xs bg-secondary text-secondary-foreground"
+        />
+        <Button type="submit" size="sm" variant="secondary">
+          <Search className="h-4 w-4" />
+          <span className="sr-only">Search</span>
+        </Button>
+      </form>
       <FilterDropdown />
     </div>
   );
